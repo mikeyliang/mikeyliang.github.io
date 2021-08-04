a9bcafc3f051afe625b86e1a394f659614ddcafa
thelostninjah <liangmichael27822@gmail.com>
2021-08-04 01:48:14 -0700

    basic layout

---
+++ assets/js/main.js
@@ -0,0 +1,71 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent=document.getElementsByClassName('skills__content'),
+    skillsHeader=document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass=this.parentNode.className
+
+    for(i=0; i<skillsContent.length; i++) {
+        skillsContent[i].className='skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className='skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs=document.querySelectorAll('[data-target]'),
+    tabContents=document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>  {
+        const target=document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContents => {
+            tabContents.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/
