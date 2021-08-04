523a9a14b4481066ccbfd3c22a7698b9cf8ecce5
thelostninjah <liangmichael27822@gmail.com>
2021-08-04 16:28:19 -0700

    lightdarkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,3 +69,38 @@ tabs.forEach(tab => {
 
 
 /*==================== DARK LIGHT THEME ====================*/
+
+const themeButton=document.getElementById('theme-button')
+const darkTheme='dark-theme'
+const lighticonTheme = 'uil-sunset'
+const darkiconTheme = 'uil-wind-moon'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+
+if (selectedTheme) {
+    if (selectedIcon == darkiconTheme) {
+        document.body.classList.toggle(darkTheme)
+        themeButton.classList.replace(darkiconTheme, lighticonTheme)
+    }
+}
+
+themeButton.addEventListener('click', () => {
+    if (document.body.classList.contains(darkTheme)) {
+        document.body.classList.toggle(darkTheme)
+        localStorage.setItem('selected-theme', 'light')
+        localStorage.setItem('selected-icon', lighticonTheme)
+    } else {
+        document.body.classList.toggle(darkTheme)
+        localStorage.setItem('selected-theme', 'dark')
+        localStorage.setItem('selected-icon', darkiconTheme)
+    }
+    themeButton.classList.toggle(darkiconTheme)
+    themeButton.classList.toggle(lighticonTheme)
+})
+
+
+
+
+
