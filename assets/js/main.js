162fe9c1927147bb6421db1d3f0ff83a732144a6
thelostninjah <liangmichael27822@gmail.com>
2021-08-06 17:21:53 -0700

    animations and format

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,25 +9,6 @@
 /* Validate if constant exists */
 
 
-/*==================== SELECTOR HELPER FUNCTION ====================*/
-(function() {
-    "use strict";
-
-    /**
-     * Easy selected helper function
-     * @returns querySelected
-     */
-    const select = (el, all = false) => {
-        el = el.trim()
-        if (all) {
-            return [...document.querySelectorAll(el)]
-        } else {
-            return document.querySelector(el)
-        }
-    }
-
-})
-
 /*==================== ACCORDION SKILLS ====================*/
 const skillsContent=document.getElementsByClassName('skills__content'),
     skillsHeader=document.querySelectorAll('.skills__header')
@@ -67,19 +48,83 @@ tabs.forEach(tab => {
 })
 
 /*==================== TYPED EFFECT ====================*/
-const typed = document.querySelector('#typed')
-if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    console.log(typed_strings)
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
-    });
+
+
+
+/* https://usefulangle.com/post/75/typing-effect-animation-javascript-css */
+
+// List of sentences
+var _CONTENT = [ 
+	"Create",
+    "Design",
+    "Develop",
+    "Innovate"
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed 
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Cursor element 
+var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
 }
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 200);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
+
+
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
