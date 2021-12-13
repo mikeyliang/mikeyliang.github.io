d06d49826f331a81e3f9f3f003e2c2a9bf7cb452
mikeyliang <liangmichael27822@gmail.com>
2021-12-13 01:35:10 -0500

    media query height

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 /*===== DISABLE ZOOM =====*/
 /* Validate if constant exists */
 
+
 window.addEventListener('keydown', function (e) {
     if ((e.ctrlKey || e.metaKey) && (e.which === 61 || e.which === 107 || e.which === 173 || e.which === 109 || e.which === 187 || e.which === 189)) {
         e.preventDefault();
