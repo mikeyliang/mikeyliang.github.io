13af0f9e3567d6aa579d0ed6f868aecc2f8fc273
mikeyliang <liangmichael27822@gmail.com>
2021-12-14 22:43:00 -0500

    fix scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,7 @@
 /* Validate if constant exists */
 
 
-/* window.addEventListener('keydown', function (e) {
-    if ((e.ctrlKey || e.metaKey) && (e.which === 61 || e.which === 107 || e.which === 173 || e.which === 109 || e.which === 187 || e.which === 189)) {
-        e.preventDefault();
-    }
-}, false); */
 
-window.onload = function () {
-    document.body.scrollIntoView(true);
-    return;
-}
 
 /*===== SCROLL SECTION =====*/
 /* Changes number of which section scroll is in */
